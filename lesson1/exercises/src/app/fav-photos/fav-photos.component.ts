34c09fd69fb6b50ba98ca3127bd043ccf2fc7032
Dominic Frisch <frisch7@gmail.com>
2020-07-26 17:00:39 -0500

    added photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image1 = 'https://www.aafsfl.org/uploads/5/7/8/0/5780701/s324222675859668953_p18_i1_w1280.jpeg';
+  image2 = 'https://scalebranding.com/wp-content/uploads/2020/07/three-peak-mountain-01.png';
+  image3 = 'https://api.time.com/wp-content/uploads/2019/09/american-flag-wind.jpg?quality=85&w=1200&h=628&crop=1';
 
   constructor() { }
 
