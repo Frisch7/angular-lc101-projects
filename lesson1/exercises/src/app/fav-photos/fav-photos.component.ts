961e851a408b4cccf2a01f82da9dea8e0098d4bd
Dominic Frisch <frisch7@gmail.com>
2020-07-26 17:37:26 -0500

    finished exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.aafsfl.org/uploads/5/7/8/0/5780701/s324222675859668953_p18_i1_w1280.jpeg';
   image2 = 'https://scalebranding.com/wp-content/uploads/2020/07/three-peak-mountain-01.png';
   image3 = 'https://api.time.com/wp-content/uploads/2019/09/american-flag-wind.jpg?quality=85&w=1200&h=628&crop=1';
